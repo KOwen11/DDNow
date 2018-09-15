ffffac6da38790a84b4d391a53d5b00044c92a8b
Jorrell <kevinowen11@gmail.com>
2018-09-15 01:02:33 +0000

    split file input and http request to separate methods

--- getResourceLists.js
+++ getResourceLists.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 const http = require('http');
 
-/*
-function httpGetRequest(rqstUrl) {
+function httpGetRequest(rqstUrl,flName) {
+  var result;
   http.get(rqstUrl, (res) => {
     const { statusCode } = res;
     const contentType = res.headers['content-type'];
@@ -21,14 +21,15 @@ function httpGetRequest(rqstUrl) {
       res.resume();
       return;
     }
-  
     res.setEncoding('utf8');
     let rawData = '';
     res.on('data', (chunk) => { rawData += chunk; });
     res.on('end', () => {
       try {
         const parsedData = JSON.parse(rawData);
-        return parsedData;
+        result = rawData;
+        fs.writeFile("./data/resourceList/"+flName+".json", rawData, "utf8");
+        console.log("write: "+flName+".json");
       } catch (e) {
         console.error(e.message);
       }
@@ -37,7 +38,6 @@ function httpGetRequest(rqstUrl) {
     console.error(`Got error: ${e.message}`);
   });
 }
-*/
 
 function init() {
   fs.readFile('./data/rL1.json', function(err, data){
@@ -48,48 +48,11 @@ function init() {
     result.list.forEach(function(elem){
       var rqstUrl = "http://www.dnd5eapi.co/api/"+elem+"/";
       console.log(rqstUrl);
-      //var jsonData = httpGetRequest(rqstUrl);
-      //console.log(jsonData);
-      //jsonData = JSON.stringify(httpGetRequest(rqstUrl));
-      //fs.writeFile("./data/resourceList/"+elem, jsonData, "utf8");
-      http.get(rqstUrl, (res) => {
-        const { statusCode } = res;
-        const contentType = res.headers['content-type'];
-      
-        let error;
-        if (statusCode !== 200) {
-          error = new Error('Request Failed.\n' +
-                            `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-          error = new Error('Invalid content-type.\n' +
-                            `Expected application/json but received ${contentType}`);
-        }
-        if (error) {
-          console.error(error.message);
-          // consume response data to free up memory
-          res.resume();
-          return;
-        }
-      
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-          try {
-            const parsedData = JSON.parse(rawData);
-            //return parsedData;
-            //jsonData = JSON.stringify(httpGetRequest(rqstUrl));
-            fs.writeFile("./data/resourceList/"+elem+".json", rawData, "utf8");
-          } catch (e) {
-            console.error(e.message);
-          }
-        });
-      }).on('error', (e) => {
-        console.error(`Got error: ${e.message}`);
-      });
+      httpGetRequest(rqstUrl, elem);
     });
   });
 }
+
 init();
 
 
